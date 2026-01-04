6dc895d839935c1fd44651b3659b43cb56ba193d
lewismvaughan <79458915+lewismvaughan@users.noreply.github.com>
2026-01-04 18:19:31 +0100

    updated config

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => {
-  const ENV = process.env.APP_ENV || 'development';
+  const ENV = process.env.EXPO_PUBLIC_ || 'dev';
 
   return {
     ...config,
